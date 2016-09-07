a70adc5cfba010941485224597982a23c2a34adc
Marat Khusnetdinov <marat@khusnetdinov.ru>
2016-09-07 21:18:43 +0300

    Fix install.sh

--- install.sh
+++ install.sh
@@ -6,14 +6,6 @@ function die()
   exit 1
 }
 
-# Add .old to any existing Vim file in the home directory
-for filepath in "${HOME}/.vim" "${HOME}/.vimrc" "${HOME}/.gvimrc"; do
-  if [ -e $filepath ]; then
-    mv "${filepath}" "${filepath}.old" || die "Could not move ${filepath} to ${filepath}.old"
-    echo "${filepath} has been renamed to ${filepath}.old"
-  fi
-done
-
 # Clone Janus into .janus
 git clone --recursive https://github.com/khusnetdinov/.janus.git "${HOME}/.janus" \
   || die "Could not clone the repository to ${HOME}/.janus"
